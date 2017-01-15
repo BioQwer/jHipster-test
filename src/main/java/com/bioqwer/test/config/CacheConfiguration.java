bd8cde098743a526084ce2b17cef52aa4ad78387
Antony Alexsandrov <af.alexsandrov@gmail.com>
2017-01-15 13:12:20 +0300

    little fix

--- src/main/java/com/bioqwer/test/config/CacheConfiguration.java
+++ src/main/java/com/bioqwer/test/config/CacheConfiguration.java
@@ -1,20 +1,23 @@
 package com.bioqwer.test.config;
 
 
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
 import org.springframework.cache.support.NoOpCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 
-import javax.annotation.PreDestroy;
-import javax.inject.Inject;
+import com.hazelcast.config.MapConfig;
 
 @Configuration
 @EnableCaching
